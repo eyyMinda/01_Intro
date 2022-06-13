0a50b37d181dfabb65720ab49fedbf975c09435f
eyyMinda <gamerx14x@gmail.com>
2022-06-13 19:16:55 +0300

    Cache

--- config.js
+++ config.js
@@ -11,6 +11,10 @@ config.dev = {
         password: 'admin',
         database: 'batai',
     },
+    cache: {
+        default: 0,
+        period: {},
+    }
 }
 
 config.prod = {
@@ -24,6 +28,25 @@ config.prod = {
         password: 'r84tr5s25e84rrg52f5er84r5ert8r4g55e',
         database: 'batai-r5fe1d15',
     },
+    cache: {
+        default: 60 * 60,
+        period: {
+            css: 60 * 60,
+            js: 60 * 60 * 6,
+            svg: 60 * 60 * 12,
+            png: 60 * 60 * 24,
+            jpg: 60 * 60 * 24,
+            ico: 60 * 60 * 24,
+            woff2: 60 * 60 * 24,
+            woff: 60 * 60 * 24,
+            ttf: 60 * 60 * 24,
+            otf: 60 * 60 * 24,
+            eot: 60 * 60 * 24,
+            webmanifest: 60 * 60 * 24,
+            pdf: 60 * 60 * 24,
+            json: 60 * 60 * 24,
+        },
+    }
 }
 
 const nodeEnv = process.env.NODE_ENV;
