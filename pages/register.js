a615b9b1415611199d7ea0315360b1d0e83b3495
eyyMinda <gamerx14x@gmail.com>
2022-06-16 21:35:34 +0300

    Register inputs auth setup

--- pages/register.js
+++ pages/register.js
@@ -5,11 +5,35 @@ class PageRegister extends PageTemplate {
         super(data);
         this.title = `┼─Register─┼`;
         this.css = '<link rel="stylesheet" href="/css/pages/register.css">';
+        this.js = 'register'
         this.isLoggedIn = data.user.isLoggedIn;
     }
 
     mainHTML() {
-        return `<h1>Register Page</h1>`
+        return `
+        <div class="row">
+            <h1>Register</h1>
+            <p>Register or leave</p>
+            <form class="form" action="/api/account" method="POST">
+                
+            <label for="fullname">Fullname</label>
+                <input id="fullname" name="fullname" placeholder="John Doe" type="text" autocomplete="name" required>
+
+                <label for="email">Email</label>
+                <input id="email" name="email" placeholder="JohnDoe@email.com"type="email" autocomplete="email" required>
+                
+                <label for="password">Password</label>
+                <input id="pass" name="pass" placeholder="yourmom69"type="password" autocomplete="new-password" required>
+                
+                <label for="repass">Repeat password</label>
+                <input id="repass" name="repass" placeholder="yourmom69"type="password" autocomplete="new-password" required>
+                
+                <span><input type="checkbox" id="tos" required>
+                <label for="tos">Agree to terms and service conditions</label></span>
+
+                <button type="submit">Register</button>
+            </form>
+        </div>`
     }
 }
 
