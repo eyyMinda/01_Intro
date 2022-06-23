9666fd328ec6acf1f6e982cb6d3a1f7521079628
eyyMinda <gamerx14x@gmail.com>
2022-06-24 01:52:36 +0300

    Creating API

--- api/account.js
+++ api/account.js
@@ -1,7 +1,26 @@
 const handler = {};
 
-handlerTypes = ['get', 'post', 'edit', 'delete'];
+handler.account = (data, res) => {
+    const acceptableMethods = ['get', 'post', 'edit', 'delete'];
+    if (acceptableMethods.includes(data.httpMethod)) {
+        return handler._account[data.httpMethod](data, res);
+    }
+    return res.end(JSON.stringify('This method is not acceptable'))
+}
 
+handler._account = {};
 
+handler._account.get = (data, res) => {
+    return res.end(JSON.stringify('Account: get'));
+}
+handler._account.post = (data, res) => {
+    return res.end(JSON.stringify('Account: post'));
+}
+handler._account.edit = (data, res) => {
+    return res.end(JSON.stringify('Account: edit'));
+}
+handler._account.delete = (data, res) => {
+    return res.end(JSON.stringify('Account: delete'));
+}
 
 export default handler;
