d0810e055648b87cf4d4326d017cb4615b8ac8a3
eyyMinda <gamerx14x@gmail.com>
2022-06-28 17:19:26 +0300

    refactor into callbacks

--- api/account.js
+++ api/account.js
@@ -1,20 +1,21 @@
 import { isValid } from '../lib/is-valid/isValid.js'
 const handler = {};
 
-handler.account = (data, res) => {
-    const acceptableMethods = ['get', 'post', 'edit', 'delete'];
+handler.account = (data, callback) => {
+    const acceptableMethods = ['get', 'post', 'put', 'delete'];
     if (acceptableMethods.includes(data.httpMethod)) {
-        return handler.innerMethods[data.httpMethod](data, res);
+        const httpMethodFunc = handler._innerMethods[data.httpMethod];
+        return httpMethodFunc(data, callback);
     }
-    return res.end(JSON.stringify('This method is not acceptable'))
+    return callback(405, { msg: 'This method is not acceptable'});
 }
 
 handler.innerMethods = {};
 
-handler.innerMethods.get = (data, res) => {
-    return res.end(JSON.stringify('Account: get'));
+handler.innerMethods.get = (data, callback) => {
+    return callback(200, { msg: 'Account: get'});
 }
-handler.innerMethods.post = (data, res) => {
+handler.innerMethods.post = (data, callback) => {
     const { payload } = data;
     const { fullname, email, pass } = payload;
     const allowedKeys = ['fullname', 'email', 'pass'];
@@ -24,23 +25,23 @@ handler.innerMethods.post = (data, res) => {
         for (const key of keys) {
             if (!allowedKeys.includes(key)) deniedKeys.push(key);
         }
-        return res.end(`Only 'fullname', 'email' and 'pass' is allowed to be posted. Denied keys: ${deniedKeys}`)
+        return callback(400, {msg:`Only 'fullname', 'email' and 'pass' is allowed to be posted. Denied keys: ${deniedKeys}`})
     }
 
     const [fullnameErr, fullnameMsg] = isValid.fullname(fullname);
-    if (fullnameErr) return res.end(JSON.stringify(fullnameMsg));
+    if (fullnameErr) return callback(400, {msg:fullnameMsg});
     const [emailErr, emailMsg] = isValid.email(email);
-    if (emailErr) return res.end(JSON.stringify(emailMsg));
+    if (emailErr) return callback(400, {msg: emailMsg});
     const [passErr, passMsg] = isValid.password(pass);
-    if (passErr) return res.end(JSON.stringify(passMsg));
+    if (passErr) return callback(400, {msg:passMsg});
 
-    return res.end(JSON.stringify('Account: post'));
+    return callback(200, { msg:'Account: post'});
 }
-handler.innerMethods.edit = (data, res) => {
-    return res.end(JSON.stringify('Account: edit'));
+handler.innerMethods.put = (data, res) => {
+    return callback(200, { msg:'Account: put'});
 }
 handler.innerMethods.delete = (data, res) => {
-    return res.end(JSON.stringify('Account: delete'));
+    return callback(200, { msg:'Account: delete'});
 }
 
 export default handler;
