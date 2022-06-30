d53bf07c80901faed99eb04616972ba6ae80b4df
eyyMinda <gamerx14x@gmail.com>
2022-07-01 00:07:13 +0300

    Accounts

--- api/account.js
+++ api/account.js
@@ -1,47 +1,99 @@
 import { isValid } from '../lib/is-valid/isValid.js'
+import { file } from '../lib/file.js';
+import { utils } from '../lib/utils.js';
 const handler = {};
 
 handler.account = (data, callback) => {
     const acceptableMethods = ['get', 'post', 'put', 'delete'];
     if (acceptableMethods.includes(data.httpMethod)) {
-        const httpMethodFunc = handler._innerMethods[data.httpMethod];
+        const httpMethodFunc = handler.innerMethods[data.httpMethod];
         return httpMethodFunc(data, callback);
     }
-    return callback(405, { msg: 'This method is not acceptable'});
+    return callback(405, { msg: 'This method is not acceptable' });
 }
 
 handler.innerMethods = {};
-
-handler.innerMethods.get = (data, callback) => {
-    return callback(200, { msg: 'Account: get'});
-}
-handler.innerMethods.post = (data, callback) => {
+////////////////////////////////POST\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+handler.innerMethods.post = async (data, callback) => {
     const { payload } = data;
+
+    const [validErr, validMsg] = utils.objectValidator(payload, {
+        required: ['fullname', 'email', 'pass'],
+    });
+    if (validErr) return callback(400, { msg: validMsg });
+
     const { fullname, email, pass } = payload;
-    const allowedKeys = ['fullname', 'email', 'pass'];
-    const keys = Object.keys(payload);
-    if (keys.length > allowedKeys.length) {
-        let deniedKeys = [];
-        for (const key of keys) {
-            if (!allowedKeys.includes(key)) deniedKeys.push(key);
-        }
-        return callback(400, {msg:`Only 'fullname', 'email' and 'pass' is allowed to be posted. Denied keys: ${deniedKeys}`})
+    const [fullnameErr, fullnameMsg] = isValid.fullname(fullname);
+    if (fullnameErr) return callback(400, { msg: fullnameMsg });
+    const [emailErr, emailMsg] = isValid.email(email);
+    if (emailErr) return callback(400, { msg: emailMsg });
+    const [passErr, passMsg] = isValid.password(pass);
+    if (passErr) return callback(400, { msg: passMsg });
+
+    const [readErr] = await file.read('accounts', email + '.json');
+    if (!readErr) return callback(400, { msg: 'Account already exists' });
+
+    delete payload.pass;
+    payload.hashedPassword = utils.hash(pass)[1];
+    payload.lastLoginDate = 0;
+    payload.registrationDate = Date.now();
+
+    const [createErr] = await file.create('accounts', email + '.json', payload);
+    if (createErr) return callback(500, { msg: 'Account was not created due to server problems, try again later' });
+
+    return callback(200, { msg: 'Account was created successfully' });
+}
+////////////////////////////////GET\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+handler.innerMethods.get = async (data, callback) => {
+    const email = data.searchParams.get('email');
+    const [emailErr, emailMsg] = isValid.email(email);
+    if (emailErr) return callback(400, { msg: emailMsg });
+    const [readErr, readMsg] = await file.read('accounts', email + '.json');
+    if (readErr) return callback(404, { msg: 'This user does not exist or no permissions to view it' });
+    const [userErr, userData] = utils.parseJSONtoObject(readMsg);
+    if (userErr) return callback(500, { msg: 'Reading user info was unsuccessfull' });
+    delete userData.hashedPassword;
+    return callback(200, { msg: userData });
+}
+////////////////////////////////PUT\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+handler.innerMethods.put = async (data, callback) => {
+    const { payload } = data;
+    const email = data.searchParams.get('email');
+    const [emailErr, emailMsg] = isValid.email(email);
+    if (emailErr) return callback(400, { msg: emailMsg });
+    const [validErr, validMsg] = utils.objectValidator(payload, { optional: ['fullname', 'pass'] });
+    if (validErr) return callback(400, { msg: validMsg });
+
+    const { fullname, pass } = payload;
+    if (fullname) {
+        const [fullnameErr, fullnameMsg] = isValid.fullname(fullname);
+        if (fullnameErr) return callback(400, { msg: fullnameMsg });
+    }
+    if (pass) {
+        const [passErr, passMsg] = isValid.password(pass);
+        if (passErr) return callback(400, { msg: passMsg });
     }
 
-    const [fullnameErr, fullnameMsg] = isValid.fullname(fullname);
-    if (fullnameErr) return callback(400, {msg:fullnameMsg});
-    const [emailErr, emailMsg] = isValid.email(email);
-    if (emailErr) return callback(400, {msg: emailMsg});
-    const [passErr, passMsg] = isValid.password(pass);
-    if (passErr) return callback(400, {msg:passMsg});
+    const [readErr, readMsg] = await file.read('accounts', email + '.json');
+    if (readErr) return callback(404, { msg: 'This user does not exist or no permissions to view it' });
+    const [parseErr, userData] = utils.parseJSONtoObject(readMsg);
+    if (parseErr) return callback(500, { msg: 'Failed to update account due to server problems, try again later' });
+    if (fullname) userData.fullname = fullname;
+    if (pass) userData.hashedPassword = utils.hash(pass)[1];
 
-    return callback(200, { msg:'Account: post'});
+    const [updateErr] = await file.update('accounts', email + '.json', userData);
+    if (updateErr) return callback(500, { msg: 'Failed to update account due to server problems, try again later' });
+
+    return callback(200, { msg: 'Account has been updated' });
 }
-handler.innerMethods.put = (data, res) => {
-    return callback(200, { msg:'Account: put'});
-}
-handler.innerMethods.delete = (data, res) => {
-    return callback(200, { msg:'Account: delete'});
+////////////////////////////////DELETE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+handler.innerMethods.delete = async (data, callback) => {
+    const email = data.searchParams.get('email');
+    const [emailErr, emailMsg] = isValid.email(email);
+    if (emailErr) return callback(400, { msg: emailMsg });
+    const [deleteErr] = await file.delete('accounts', email + '.json', userData);
+    if (deleteErr) return callback(500, { msg: 'Failed to delete an account due to server problems, try again later' });
+    return callback(200, { msg: 'Account has been deleted' });
 }
 
 export default handler;
